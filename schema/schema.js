9f4394a781b9fc07a966fcc4e2cd02bca3ea991c
faheelsattar <faheelsattar@yahoo.com>
2020-06-27 22:12:48 +0500

    Add relation of movie with the producer

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,42 @@
 const graphql = require("graphql")
 const _ = require("lodash")
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql
 
 //dummy data for now
 let moviestore=[
-    {id:"1", name:"Hulk", genre:"action"},
-    {id:"2", name:"Ironman", genre:"action"},
-    {id:"3", name:"Thor", genre:"action"},
+    {id:"1", name:"Hulk", genre:"action", producerid:"1"},
+    {id:"2", name:"Ironman", genre:"action",producerid:"2"},
+    {id:"3", name:"Thor", genre:"action",producerid:"3"},
 ]
+
+let producerStore=[
+    {id:"1", name:"Norman", age: 32},
+    {id:"2", name:"Clint", age:24},
+    {id:"3", name:"Rug", age: 56},
+]
+
 const movietype = new GraphQLObjectType({
     name: "movie",
     fields:()=>({
         id:{type: GraphQLID},
         name: {type: GraphQLString},
-        genre:{type: GraphQLString}
+        genre:{type: GraphQLString},
+        producer:{
+            type:producertype,
+            resolve(parent, args){
+                return _.find(producerStore, {id: parent.producerid})
+            }
+        }
+    })
+})
+
+const producertype = new GraphQLObjectType({
+    name: "producer",
+    fields:()=>({
+        id:{type: GraphQLID},
+        name: {type: GraphQLString},
+        age:{type: GraphQLInt}
     })
 })
 
@@ -27,6 +49,13 @@ const rootquery= new GraphQLObjectType({
             resolve(parent, args){
                 //prvoisison on grapiql
                 return _.find(moviestore, {id: args.id})
+            }
+        },
+        producer:{
+            type:producertype,
+            args:{id:{type:GraphQLID}},
+            resolve(parent, args){
+                return _.find(producerStore, {id:args.id})
             }
         }
     }
